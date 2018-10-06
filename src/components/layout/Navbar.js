37fd3923ab5c212ba3eb5fa1c6d8549e923cae04
Matan <matanbe18@gmail.com>
2018-10-06 10:34:48 +0300

    Fixed routing issue for gh pages ...

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark mb-4">
-      <Link className="main-link" to="/">
+      <Link className="main-link" to={process.env.PUBLIC_URL + '/'}>
         <i className="fas fa-headphones-alt mr-2" /> 
 
         <span className="navbar-brand mb-0 h1 mx-auto"> Lyrical </span>
